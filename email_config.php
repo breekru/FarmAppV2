ce424d4f2fa0853932af03e300b222460d313cf8
breekru <breekru@gmail.com>
2025-05-21 20:09:27 -0500

    Update email_config.php

--- email_config.php
+++ email_config.php
@@ -8,4 +8,7 @@ define('SMTP_SECURE', 'tls'); // 'tls' or 'ssl'
 define('FROM_EMAIL', 'noreply@blkfarms.com');
 define('FROM_NAME', 'FarmApp');
 define('REPLY_TO_EMAIL', 'noreply@blkfarms.com');
+
+// Debug Settings (set to false in production)
+define('SMTP_DEBUG', false);                             // Enable SMTP debugging
 ?>
